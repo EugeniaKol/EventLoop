e04f877ece1dee4390fba379322f8de52ecd4d3c
Polina <schachowa.paulina@gmail.com>
2020-12-25 07:17:38 +0200

    Update eventloop.go

--- eventloop.go
+++ eventloop.go
@@ -1,5 +1,7 @@
 package main
-
+import (
+  "sync"
+)
 //print
 //add 2 5
 
@@ -11,22 +13,105 @@ type Handler interface {
 	Post(cmd Command)
 }
 
+
+type messageQueue struct {
+	sync.Mutex
+
+	data []Command
+	receiveSignal chan struct{}
+	receiveRequested bool
+}
+
+func (mq *messageQueue) push(command Command) {
+	mq.Lock()
+	defer mq.Unlock()
+
+	mq.data = append(mq.data, command)
+	if mq.receiveRequested {
+		mq.receiveRequested = false
+		mq.receiveSignal <- struct{}{}
+	}
+	
+}
+
+func (mq *messageQueue) pull() Command {
+	mq.Lock()
+	defer mq.Unlock()
+
+	if mq.empty(){
+		mq.receiveRequested = true
+		mq.Unlock()
+		<- mq.receiveSignal
+		mq.Lock()
+	}
+
+	res := mq.data[0]
+	mq.data[0] = nil
+	mq.data = mq.data[1:]
+	return res
+}
+
+func (mq *messageQueue) empty() bool{
+	return len(mq.data) == 0
+}
+
+
 type Loop struct {
+	mq *messageQueue
+
+	stopSignal chan struct{}
+	stopRequest bool
+}
+
+func(l *Loop) Start(){
+	l.mq = &messageQueue{ receiveSignal: make(chan struct{})}
+	l.stopSignal = make(chan struct{})
+	go func() {
+		for !l.stopRequest || !l.mq.empty() {
+			cmd := l.mq.pull()
+			cmd.Execute(l)
+		}
+		l.stopSignal <- struct{}{}
+	}()
 
 }
 
-func(l Loop) Start(){
-
+func (l *Loop) Post(cmd Command) {
+	l.mq.push(cmd)
+//cmd.Execute(l)
 }
 
-func (l Loop) Post(cmd Command) {
+type CommandFunc func (h Handler)
 
+func (cf CommandFunc) Execute(h Handler) {
+	cf(h)
 }
 
-func (l Loop) AwaitFinish() {
-
+func (l *Loop) AwaitFinish() {
+	l.Post(CommandFunc(func(h Handler) {
+		l.stopRequest = true
+	}))
+	
+	<- l.stopSignal
 }
 
-//func main(){
+func main() {
 	//use parser, start loop, post parsed commands in a cycle and await finish
-//}
+	var l Loop
+
+	l.Start()
+
+
+//	for cmd := range parsedCommand {
+//		l.Post(cmd)
+//	}
+
+
+
+	l.Post(&printCmd{msg: "hello"})
+	l.Post(&addCmd{a:4, b:2})
+	l.Post(&printCmd{msg: "hello2"})
+
+	l.AwaitFinish()
+
+}
